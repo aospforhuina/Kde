5978fcf5d608c7686239b1bb35e8b0686dd4ca11
aospforhuina <castleclimb6@gmail.com>
2026-01-28 17:31:06 +0900

    Add installation script for Arch Linux environment

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo pacman -S xorg-server xorg-xinit git nano plasma-meta kde-applications-meta firefox fcitx5 fcitx5-hangul fcitx5-configtool plasma-x11-session adobe-source-han-sans-kr-fonts sddm && sudo systemctl enable --now sddm 
+
