8bc17c6787bc0f95c77c4e146d6581a1d30041c9
aospforhuina <castleclimb6@gmail.com>
2026-01-30 20:04:51 +0900

    Enhance install.sh with new packages and instructions
    Updated install script to include additional packages and modified environment variable setup instructions.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ read -p "This script will install KDE, SDDM, and Fcitx5 on this device. Do you w
 if [ "$answer" == "y" ]; then
     # 1. 시스템 업데이트 및 필수 패키지 설치
     sudo pacman -Syu --noconfirm kde-applications-meta plasma-meta sddm \
-    fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-gtk fcitx5-qt \
+    fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-gtk fcitx5-qt nano \
     noto-fonts noto-fonts-cjk noto-fonts-emoji git base-devel fcitx5 && \
 
     sudo systemctl enable --now sddm && \
@@ -18,13 +18,16 @@ if [ "$answer" == "y" ]; then
     yay -S --noconfirm otf-pretendard && \
 
     # 5. 환경 변수 설정 (System-wide)
-    sudo bash -c "cat >> /etc/environment <<EOF
-GTK_IM_MODULE=fcitx
-QT_IM_MODULE=fcitx
-XMODIFIERS=@im=fcitx
-EOF"
+    # sudo bash -c "cat >> /etc/environment <<EOF
+#GTK_IM_MODULE=fcitx
+#QT_IM_MODULE=fcitx
+#XMODIFIERS=@im=fcitx
+#EOF"
 
-    echo """done! u can add hangul in fcitx5 and u need to apply virtual keyboard setup(fcitx) too"""
+    echo """done! u can add hangul in fcitx5 and u need to apply virtual keyboard setup(fcitx) too
+    you need to add system variable to /etc/enviroment copy variable to below. \n\nGTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx
+XMODIFIERS=@im=fcitx""
 
 elif [ "$answer" == "n" ]; then
     echo "bye"
