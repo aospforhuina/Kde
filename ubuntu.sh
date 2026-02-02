ba199009802a46752f3be0d105fa1fc80fbd63f1
aospforhuina <castleclimb6@gmail.com>
2026-02-02 14:33:16 +0900

    Add script to install KDE on Ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# -p 옵션을 사용해야 질문 메시지를 출력하고 입력을 받을 수 있습니다.
+read -p "This script will install KDE on your device. Continue? (y/n): " answer
+
+if [ "$answer" == "y" ]; then
+    sudo apt update
+    # sddm은 kde-full에 포함되어 있지만 명시해도 무방합니다.
+    sudo apt install -y kde-full sddm language-pack-ko fonts-nanum* fontconfig
+    
+    # sddm을 기본 서비스로 설정하고 즉시 실행
+    sudo systemctl enable --now sddm
+    
+    echo "KDE installation complete. Please reboot if the screen doesn't switch."
+
+elif [ "$answer" == "n" ]; then
+    echo "Installation cancelled. Bye!"
+
+else
+    echo "Invalid input. You can only type y or n."
+fi
