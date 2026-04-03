d3a9e03eb4255d93339ef65c50b4ba02caac9c3f
aospforhuina <castleclimb6@gmail.com>
2026-04-03 19:20:15 +0900

    for debian

--- install_debian.sh
+++ install_debian.sh
@@ -1,1 +1,19 @@
+!/bin/bash 
+
+set -e
+
+pack="kde-standard konsole  firefox fcitx5 fcitx5-hangul sddm ssh sddm-theme-breeze fcitx5-configtool im-config" 
+
+sudo apt install $pack -y
+
+sudo systemctl enable --now sddm ssh 
+im-config -n fcitx5
+
+echo "pls set this Variable on ur /etc/environment/
+XMODIFIERS=@im=fcitx
+GTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx "
+
+
+
 
