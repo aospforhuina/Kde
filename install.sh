73f06e64faff07085250d79db39a163c44617c64
aospforhuina <castleclimb6@gmail.com>
2026-06-14 02:11:33 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,18 @@ read -p "This script will install KDE, SDDM, and Fcitx5 on this device. Do you w
 
 if [ "$answer" == "y" ]; then
     # 1. 시스템 업데이트 및 필수 패키지 설치
-    sudo pacman -Syu --noconfirm kde-applications-meta plasma-meta sddm \
-    fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-gtk fcitx5-qt nano \
-    noto-fonts noto-fonts-cjk noto-fonts-emoji git base-devel fcitx5 \
-    mesa xf86-video-amdgpu vulkan-radeon && \
+    sudo pacman -Syu --noconfirm \
+    plasma-meta kde-applications-meta sddm \
+    networkmanager plasma-nm bluez bluez-utils bluedevil plasma-pa \
     pipewire pipewire-pulse pipewire-alsa wireplumber \
+    fcitx5 fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-gtk fcitx5-qt \
     fcitx5-frontend-qt5 fcitx5-frontend-gtk3 \
-
+    mesa xf86-video-amdgpu vulkan-radeon xorg-xwayland \
+    noto-fonts noto-fonts-cjk noto-fonts-emoji \
+    unzip p7zip unrar firefox git base-devel nano && \
     sudo systemctl enable --now sddm && \
+    sudo systemctl enable --now NetworkManager && \
+    sudo systemctl enable --now bluetooth && \
 
     # 2. yay 설치 (이미 폴더가 있으면 에러 날 수 있으므로 /tmp에서 작업 권장)
     cd /tmp && \
