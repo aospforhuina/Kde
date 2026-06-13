9343b4eb3f3e94cabb76cb8fe904b47a0a210613
aospforhuina <castleclimb6@gmail.com>
2026-06-14 03:01:49 +0900

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-pack="kde-standard konsole  firefox fcitx5 fcitx5-hangul sddm ssh sddm-theme-breeze fcitx5-configtool im-config" 
+pack="kubuntu-desktop network-manager plasma-nm pipewire polkit-kde-agent-1 plasma-pa konsole dolphin plasma-systemmonitor btop gparted p7zip-full p7zip-rar rar unrar unzip zip bzip2 spectacle gwenview kcalc kfind firefox fcitx5 fcitx5-hangul sddm ssh sddm-theme-breeze kde-config-fcitx5 im-config bluez bluez-tools bluedevil pipewire-module-bluetooth"
 
 sudo apt install $pack -y
-
-sudo systemctl enable --now sddm ssh 
-
+sudo apt install -f
+sudo systemctl enable --now sddm 
+sudo systemctl enable --now ssh
+sudo systemctl enable --now bluetooth
+sudo systemctl set-default graphical.target
 mkdir -p ~/.config/autostart && cp /usr/share/applications/org.fcitx.Fcitx5.desktop ~/.config/autostart
 echo "pls set this Variable on ur /etc/environment/
 XMODIFIERS=@im=fcitx
