267249e383d38afcbac76377dae745c059059d52
aospforhuina <castleclimb6@gmail.com>
2026-06-29 19:30:13 +0900

    Add font packages to installation script

--- install_debian.sh
+++ install_debian.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-pack="kubuntu-desktop network-manager plasma-nm pipewire polkit-kde-agent-1 plasma-pa konsole dolphin plasma-systemmonitor btop gparted p7zip-full rar unrar unzip zip bzip2 gwenview kcalc kfind firefox fcitx5 fcitx5-hangul sddm ssh sddm-theme-breeze kde-config-fcitx5 im-config bluez bluez-tools bluedevil"
+pack="kubuntu-desktop network-manager plasma-nm pipewire polkit-kde-agent-1 plasma-pa konsole dolphin plasma-systemmonitor bluez fonts-nanum fonts-noto-cjk  btop gparted p7zip-full rar unrar unzip zip bzip2 gwenview kcalc kfind firefox fcitx5 fcitx5-hangul sddm ssh sddm-theme-breeze kde-config-fcitx5 im-config bluez bluez-tools bluedevil"
 
 sudo apt install $pack -y
 sudo apt install -f
