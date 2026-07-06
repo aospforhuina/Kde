f6e00d57935ebf986c4be64d5e72b28e52124caa
aospforhuina <castleclimb6@gmail.com>
2026-07-06 16:26:00 +0900

    Modify install script for Hyprland setup
    Updated the installation script to install Hyprland instead of KDE and added necessary configurations for Wayland.

--- install_arch.py
+++ install_arch.py
@@ -1,35 +1,39 @@
 #!/bin/bash
 set -eu
-read -p "This script will install KDE, SDDM, and Fcitx5 on this device. Do you want to continue? (y/n): " answer
+read -p "This script will install hyprland and Fcitx5 on this device. Do you want to continue? (y/n): " answer
 
 if [ "$answer" == "y" ]; then
     # 1. 시스템 업데이트 및 필수 패키지 설치
     sudo pacman -Syu --noconfirm \
     sddm \
-    networkmanager  bluez bluez-utils bluedevil \
+    networkmanager waybar hyprland rofi-wayland bluez bluez-util \
     pipewire pipewire-pulse pipewire-alsa wireplumber \
     fcitx5 fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-gtk fcitx5-qt \
-    mesa xf86-video-amdgpu vulkan-radeon xorg-xwayland \
     noto-fonts noto-fonts-cjk noto-fonts-emoji \
     unzip p7zip unrar firefox git base-devel nano && \
+    kitty pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber bluez bluez-utils blueman brightnessctl grim slurp wl-clipboard pavucontrol && \
     sudo systemctl enable --now sddm && \
     sudo systemctl enable --now NetworkManager && \
     sudo systemctl enable --now bluetooth && \
-    sudo pacman -S --noconfirm --needed kitty pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber bluez bluez-utils blueman brightnessctl grim slurp wl-clipboard pavucontrol
-    # 2. yay 설치 (이미 폴더가 있으면 에러 날 수 있으므로 /tmp에서 작업 권장)
-    cd /tmp && \
-    git clone https://aur.archlinux.org/yay.git && \
-    cd yay && \
-    makepkg -si --noconfirm && \
-    yay -S --noconfirm otf-pretendard && \
-
-    echo "xrandr --output HDMI-1 --mode 1920x1080 --rate 60" >> ~/.xprofile
+    
+    mkdir -p ~/.config/hypr
+    echo "monitor=HDMI-A-1,1920x1080@60,0x0,1" > ~/.config/hypr/hyprland.conf
 
     echo """done! u can add hangul in fcitx5 and u need to apply virtual keyboard setup(fcitx) too
     you need to add system variable to /etc/enviroment copy variable to below.
     GTK_IM_MODULE=fcitx
     QT_IM_MODULE=fcitx
-    XMODIFIERS=@im=fcitx"""
+    XMODIFIERS=@im=fcitx
+    QT_QPA_PLATFORM=wayland
+    ELECTRON_OZONE_PLATFORM_HINT=wayland"""
+    
+    cd /tmp 
+    git clone https://aur.archlinux.org/yay.git 
+    cd yay 
+    makepkg -si --noconfirm 
+    yay -S --noconfirm otf-pretendard  
+
+    reboot
 
 elif [ "$answer" == "n" ]; then
     echo "bye"
